791cd7d635e778ccfb88dc9213066cb0a5fbe6e1
carl <c@wireless-10-145-222-6.public.utexas.edu>
2017-09-15 11:36:01 -0500

    time check fix

--- kz_herm_cpm.py
+++ kz_herm_cpm.py
@@ -4,7 +4,7 @@ import scipy as sc
 from config import *
 import new_dd as dd
 from ev_diags import *
-from datetime import datetime
+import time as t2
 from spectra import *
 from nlt_diags import *
 #from landau_tests import *
@@ -36,13 +36,37 @@ ntime=iend-istart+1
 
 
 entn_sum=np.zeros((par['nv0'],11),dtype='float')
-stamp = datetime.now().time()
+stamp = t2.time()
 for i in range(istart,iend+1):
     print 'time=',time[i],' of ',time[iend]
-    print 'time left:', (datetime.now().time()-stamp)/(time[i])*(time[i]-time[iend])
+    print 'time left:', (t2.time()-stamp)/(time[i])*(time[i]-time[iend])
     gt0=dd.read_time_step_g(i)
     gt0=np.reshape(gt0,(par['nkx0'],par['nky0'],par['nkz0'],par['nv0']),order='F')
+    gt1= gt0
+##################################
 
+    dist = g_in[x,y,:,:] #distribution function: 2d slice of 4d g_in
+
+    #make a new 2d array with the same dimensions as dist_
+    shape = np.shape(dist)
+    dist_t = np.empty(shape)
+
+    for i in range(len(kz)):
+        for j in range(len(hermiteNumbers)):
+    	dist_t[i,j] = (1j*np.sign(kz[i]))**hermiteNumbers[j] * dist[i,j]
+
+    dist_tp = np.empty(shape)
+    dist_tm = np.empty(shape)
+
+    for i in range(len(kz)):
+        for j in range(len(hermiteNumbers)+1):
+    	if j < (len(hermiteNumbers)-1):
+    		dist_tp[i,j]= (dist_t[i,j]+dist_t[i,j+1])/2
+    		dist_tm[i,j]= (dist_t[i,j]-dist_t[i,j+1])/2
+
+
+
+##################################        
     #Entropy
     entn_sum[:,10]=entn_sum[:,10]+get_entropy_hermite(gt0,kzind=-1,include_kz0=include_kz0)
     for k in range(10):
