ed80cecba978de8cf8af2e2aa159d0de10fa0268
carl <c@carls-MacBook-Air.local>
2017-09-15 16:49:52 -0500

    added back average over time

--- kz_herm_cpm.py
+++ kz_herm_cpm.py
@@ -78,8 +78,8 @@ for i in range(istart,iend+1):
         entnm_sum[:,k]= entn_sum[:,k]+dd.get_entropy_hermite(g_tm,kzind=kzindex)
 
 entn_sum=entn_sum/float(ntime)
-entnp_sum=entnp_sum
-entnm_sum=entnm_sum
+entnp_sum=entnp_sum/float(ntime)
+entnm_sum=entnm_sum/float(ntime)
 
 plt.loglog(herm_grid,prefactor*entn_sum[:,10],basex=10,basey=10)
 
