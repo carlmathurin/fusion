c42394aad7e64f6c2d75973c4b0c3e5f636369b5
carl <c@carls-MacBook-Air.local>
2017-09-18 19:04:20 -0500

    time/slope lines

--- kz_v_herm.py
+++ kz_v_herm.py
@@ -17,7 +17,7 @@ par['diagdir']="\'"+diagdir+"\'"
 time = dd.get_time_from_gout()
 kx,ky,kzgrid,herm_grid = dd.get_grids()
 
-start_time=time[0]
+start_time=time[100]
 end_time=time[len(time)-1]
 if start_time >= end_time:
     stop
@@ -69,6 +69,11 @@ for k in range(10):
     kz0=kzgrid[k*par['nkz0']/20]
     plt.loglog(herm_grid,prefactor*entn_sum[:,k],basex=10,basey=10,label=\
               plabel+' (k_z='+str(kzgrid[k*par['nkz0']/20])+')')
+    temp=temp/(herm_grid**(-1))[20]
+    plt.loglog(herm_grid,2.0*temp*herm_grid**(-1),'--',basex=10,basey=10,label=str(-1))
+    temp=prefactor*entn_sum[20,10]
+    temp=temp/(herm_grid**(-1.5))[20]
+    plt.loglog(herm_grid,2.0*temp*herm_grid**(-1.5),'--',basex=10,basey=10,label=str(-3.5))
     plt.xlabel('Hermite n')
     plt.ylabel(plabel)
     plt.legend(loc='lower left')
