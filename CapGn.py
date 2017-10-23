af179c0a406be1021ef465ea81a2cd1e71ad6936
carl <c@wireless-10-147-48-131.public.utexas.edu>
2017-10-23 14:12:38 -0500

    testing on ls5

--- CapGn.py
+++ CapGn.py
@@ -23,9 +23,7 @@ CapG = np.zeros(size)
 for j in range(10):
     D = (n[j]*nu**2/4 + 1)**(.5)
     npl = D + n[j]**(.5)*nu/2
-    CapG[j] = (- 1j*np.sign(kz))**n[j]
-     #* np.exp((n[j]**(.5)*D/(np.absolute(nu))) * (npl)**(-np.sign(kz*(n[j]+2*nu**(-2)-.5))/(n[j]**(.25)*D**(.5))
-
+    CapG[j] = (-1*np.sign(kz))**n[j]* np.exp((n[j]**(.5))*D/(np.absolute(nu))) \
+    * (npl)**(-np.sign(kz)*(n[j]+2*nu**(-2)-.5))/(n[j]**(.25)*D**(.5))
 #CapG = (- 1j*np.sign(kz))**n[j] * np.exp((n[j]**(.5)*D/(np.absolute(nu)))* \
 #(Npl)**(-np.sign(kz*(n[j]+2*nu**(-2)-.5))/(n[j]**(.25)*D**(.5))
-print CapG
