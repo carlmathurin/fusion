73e2745a4e54df6e64220563959892267b717558
carl <c@carls-MacBook-Air.local>
2017-11-27 22:23:41 -0600

    testing

--- new_dd.py
+++ new_dd.py
@@ -4862,8 +4862,7 @@ def get_entropy_hermite2(g_in,i,j):
 
     entropy=2.0*np.real(0.5*np.pi*np.sum(np.sum(np.conj(g_perp[1:,:])*g_perp[1:,:],axis=0),axis=0))
     entropy=entropy+np.real(0.5*np.pi*np.sum(np.conj(g_perp[0,:])*g_kz[0,:],axis=0))
-
-   return entropy
+    return entropy
 
 def get_entropy_hermiten(g_in,kzind=-1,include_kz0=True):
    """Gets entropy summed over kx,ky,kz but resolved in Hermite n.
