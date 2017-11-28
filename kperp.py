a23976914b5711f78963b3bd67039cfca768da78
carl <c@carls-MacBook-Air.local>
2017-11-27 21:15:11 -0600

    testing

--- kperp.py
+++ kperp.py
@@ -21,11 +21,9 @@ kx,ky,kzgrid,herm_grid = dd.get_grids()
 
 start_time=time[100]
 k_bin = [.15 + i*.15 for i in range(10)]
-kxind=np.empty(10)
-kyind=np.empty(10)
+k_bin=np.empty(10)
 for i in range(10):
-    kxind[i]=i*par['nkx0']/20
-    kyind[i]=i*par['nky0']/20
-print 'kxindex' , kxind
-print 'kyindex' , kyind
+    k_bin[i]= .15*i
 print 'k perp bins', k_bin
+print 'ky', ky
+print 'kx', kx
