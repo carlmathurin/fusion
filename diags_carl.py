91027ec9bcf08a548cfa54285b92bb5f38177882
carl <c@carls-MacBook-Air.local>
2017-12-04 21:27:32 -0600

    fix

--- diags_carl.py
+++ diags_carl.py
@@ -93,7 +93,6 @@ for i in range(len(kz)):
          #(npl)**((1)*(n[i]+2*nu**(-2)-.5))\
         #/(n[i]**(.25)*D**(.5)))\
         *(1j*np.sign(kz[j]))**n[i]
-    print 'result: ', result
     Cap2G[i] = result * np.conjugate(result) #CapG[j,i]* np.conjugate(CapG[j,i])
     #print (-1j*np.sign(kz[j]))**n[i]
     #print (-1*np.sign(kz[j]))**n[i]
