5eaf086bb40f961d2fed78cbb3dd1aa598bbf96e
carl <c@carls-MacBook-Air.local>
2018-05-29 14:59:09 -0500

    fix

--- kperpnewtest.py
+++ kperpnewtest.py
@@ -228,8 +228,8 @@ for j in range(15):
 #temp=prefactor*entn_sum[20,10]
 #temp=temp/(herm_grid**(-1.5))[20]
 #plt.loglog(herm_grid,2.0*temp*herm_grid**(-3.5),'--',basex=10,basey=10,label=str(-3.5))
-plt.loglog(herm_grid, (10**-b1)*herm_grid**(m1),'--',basex=10,basey=10,label='n^(-1.75)')
-plt.loglog(herm_grid, (10**-b2)*herm_grid**(m2),'--',basex=10,basey=10,label=('n^(%.4f)'% m))
+plt.loglog(herm_grid, (10**-float(b1)*herm_grid**(float(m1)),'--',basex=10,basey=10,label='n^(-1.75)')
+plt.loglog(herm_grid, (10**-float(b2)*herm_grid**(float(m2)),'--',basex=10,basey=10,label=('n^(%.4f)'% m))
 plt.legend(loc='lower left')
 plt.show()
 ## slope for E+ ~1.75
