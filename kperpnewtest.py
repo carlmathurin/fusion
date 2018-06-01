df0922e1644c852b5d602764a92eccc8bf4e72e9
carl <c@carls-MacBook-Air.local>
2018-06-01 16:37:47 -0500

    test

--- kperpnewtest.py
+++ kperpnewtest.py
@@ -140,6 +140,8 @@ entn_sum=entn_sum/float(ntime)
 entnp_sum=entnp_sum/float(ntime)
 entnm_sum=entnm_sum/float(ntime)
 
+print "ent+:", entnp_sum
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~TESTING BLOCK (fixed k_perp)~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #First k_perp
 plt.figure(1)
