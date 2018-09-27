11d2475faacb89c61eedf9e1d3c6b8c622d345d3
Carl M <carl@carls-air.home>
2018-09-26 19:59:32 -0500

    testing

--- carl_eigen.py
+++ carl_eigen.py
@@ -23,6 +23,7 @@ kx,ky,kz,herm_grid = dd.get_grids()
 Gamma_0 = mat.get_gamma0()
 print 'gamma: ', np.shape(Gamma_0), Gamma_0
 print 'nu: ', par['nu']
+print 'kx = ', kx[20],'ky = ', ky[20], 'kz = ', kz[20]
 us_matrix = mat.matrix(kx[20],ky[20],kz[20],Gamma_0[20],par['nu'])
 
 print us_matrix
