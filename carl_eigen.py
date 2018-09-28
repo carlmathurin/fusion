336ec71f4dae9c591251a05d64202a559d9de1c9
Carl M <carl@wireless-10-147-70-210.public.utexas.edu>
2018-09-28 12:47:24 -0500

    test

--- carl_eigen.py
+++ carl_eigen.py
@@ -54,7 +54,7 @@ f = open('g_data.txt','w+')
 print g_calc[2].real, g_calc[2].imag
 f.write('n      |G0      |G_calc')
 for i in range(48):
-    f.write('%d     |%f + i(%f)      |%f + i(%f) \n' % i,  g0[i].real, g0[i].imag, g_calc[i].real, g_calc[i].imag )
+    f.write('%d     |%f + i(%f)      |%f + i(%f) \n' %( i,  g0[i].real, g0[i].imag, g_calc[i].real, g_calc[i].imag ))
 f.close()
 # w is freq/eigenvalues
 print 'Matrix: ', np.shape(us_matrix)
