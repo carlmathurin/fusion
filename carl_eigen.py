4d00b1897835666a65d121b46dcd01250de41566
Carl M <carl@wireless-10-147-18-110.public.utexas.edu>
2018-10-01 09:06:22 -0500

    testing

--- carl_eigen.py
+++ carl_eigen.py
@@ -29,7 +29,7 @@ print 'nu: ', par['nu']
 print 'kx = ', kx[5],'ky = ', ky[5], 'kz = ', kz[5] , 'Gamma_0(index/value) [5,5]/',Gamma_0[5,5]
 us_matrix = mat.matrix(kx[5],ky[5],kz[5],Gamma_0[5,5],par['nu'])
 freq , growth, evec = mat.get_spectrum(kx[5],ky[5],kz[5],Gamma_0[5,5],par['nu'])
-print 'freq: ',np.shape(freq),', growth: ', np.shape(growth),', evec: ',np.shape(evec), evec[:,0]
+print 'freq: ',np.shape(freq), freq, ', growth: ', np.shape(growth),', evec: ',np.shape(evec), evec[:,0]
 
 g0 = evec[:,0]
 print 'g1 = ', g0[1]
