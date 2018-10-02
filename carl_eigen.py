2895913a5355cdf933de751817e87ba3acc45fed
Carl M <carl@wireless-10-147-156-153.public.utexas.edu>
2018-10-02 13:44:45 -0500

    test

--- carl_eigen.py
+++ carl_eigen.py
@@ -32,8 +32,8 @@ omega, freq , growth, evec = mat.get_spectrum(kx[5],ky[5],kz[5],Gamma_0[5,5],par
 print 'freq: ',np.shape(freq), freq, ', growth: ', np.shape(growth),', evec: ',np.shape(evec), evec[:,0]
 print 'eval: ', omega
 
-g0 = evec[:,0]
-print 'g1 = ', g0[1]
+g_0 = evec[:,0]
+print 'g1 = ', g_0[1]
 
 #0 = (nu_bar*n - 1j*w/kz)*g(n) + 1j*(sqrt(n+1)*g(n+1) + sqrt(n)*g(n-1))
 nmax = 48
@@ -42,23 +42,23 @@ g_calc = np.empty(48,dtype=complex)
 
 for n in range(48):
     if n == 0 or n == 1 :
-        g_calc[0] = g0[0]
-        g_calc[1] = g0[1]
+        g_calc[0] = g_0[0]
+        g_calc[1] = g_0[1]
     else:
-        g_calc[n] = ( (omega[0]/kz[5] + 1j*nu_bar*(n-1))*g0[n-1] - (n-1)**(.5)*g0[n-2])/ ((n)**(.5))
+        g_calc[n] = ( (omega[0]/kz[5] + 1j*nu_bar*(n-1))*g_0[n-1] - (n-1)**(.5)*g_0[n-2])/ ((n)**(.5))
 
 print 'g2 matrix vs calc: vs error '
 error = np.empty(48,dtype=complex)
 error2 = np.empty(48,dtype=complex)
 for i in range(48):
-    error[i] = abs(g0[i] - g_calc[i])/ (abs(g0[n]*g_calc[i])**.5)
-    error2[i] = abs(g0[i] - g_calc[i])/ (abs(g0[n]))
-    print 'n =', i,'  ', g0[i], 'vs', g_calc[i] , '  error =', error[i]
+    error[i] = abs(g_0[i] - g_calc[i])/ (abs(g_0[n]*g_calc[i])**.5)
+    error2[i] = abs(g_0[i] - g_calc[i])/ (abs(g_0[n]))
+    print 'n =', i,'  ', g_0[i], 'vs', g_calc[i] , '  error =', error[i]
 
 print 'herm ',np.shape(herm_grid[0:48]), 'g_calc ', np.shape(g_calc)
 print 'herm :', herm_grid[0:48]
-plt.loglog(herm_grid[0:48],g_calc, basex=10, basey=10 ,label = 'calc')
-plt.loglog(herm_grid[0:48],g0, basex=10, basey=10,label= 'matrix')
+plt.loglog(herm_grid[0:48],g_calc,'b',label = 'calc')
+plt.loglog(herm_grid[0:48],g_0,'r',label= 'matrix')
 plt.legend()
 #label= 'k_p ='+str(k_bin[j])
 plt.show()
@@ -77,9 +77,9 @@ plt.show()
 """
 f = open('g_data.txt','w+')
 print g_calc[2].real, g_calc[2].imag
-f.write('n      |G0             |G_calc\n')
+f.write('n      |g_0             |G_calc\n')
 for i in range(48):
-    f.write('%d     |%e + i(%e)      |%e + i(%e) \n' %( i,  g0[i].real, g0[i].imag, g_calc[i].real, g_calc[i].imag ))
+    f.write('%d     |%e + i(%e)      |%e + i(%e) \n' %( i,  g_0[i].real, g_0[i].imag, g_calc[i].real, g_calc[i].imag ))
 f.close()
 # w is freq/eigenvalues
 """
