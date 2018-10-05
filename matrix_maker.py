4b3fc2f88b7fc8ebe916b2553440bc6967dd3476
Carl M <carl@carls-air.home>
2018-10-05 14:32:54 -0500

    test

--- matrix_maker.py
+++ matrix_maker.py
@@ -17,8 +17,8 @@ from scipy import interpolate
 nu = 0.00222;
 
 par = {'omt':0, 'omn':0, 'Ti0Te':1.0, 'kxmin':0.05, 'kxmax0':1.55, 'kymin':0.05, 'kymax0':1.55, 'kzmin':0.1, 'kzmax0':3.1, 'nkx0':32, 'nky0':64, 'nkz0':64,
-       'nv0':48, 'nh0':1, 'nspec':1, 'hyp_x':1.75, 'hyp_y':1.75, 'hyp_z':0.0, 'hypx_order':8, 'hypy_order':8, 'hypz_order':8, 'hyp_v':2.0, 'hypv_order':8, 'hyp_conv':1.0,
-       'num_k_hyp_conv':1, 'hyp_conv_ky':False, 'np_herm':24, 'np_kz':1, 'np_hank':1, 'np_spec':1, 'hyp_nu':0.1, 'nuno_closure':True, 'em_conserve':True}
+       'nv0':48, 'nh0':1, 'nspec':1, 'hyp_x':0, 'hyp_y':0, 'hyp_z':0.0, 'hypx_order':0, 'hypy_order':0, 'hypz_order':0, 'hyp_v':0, 'hypv_order':0, 'hyp_conv':0,
+       'num_k_hyp_conv':0, 'hyp_conv_ky':False, 'np_herm':24, 'np_kz':1, 'np_hank':1, 'np_spec':1, 'hyp_nu':0, 'nuno_closure':True, 'em_conserve':True}
 
 #changed omn from 1.0 to 0 and omt from 10.0 to 0
 
