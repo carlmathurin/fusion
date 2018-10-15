0cbd452abffb23e23a9888effa4afa9069b75ee5
Carl M <carl@wireless-10-145-21-203.public.utexas.edu>
2018-10-15 10:03:48 -0500

    test

--- carl_eigen.py
+++ carl_eigen.py
@@ -59,6 +59,8 @@ for i in range(48):
 print 'herm ',np.shape(herm_grid[0:48]), 'g_calc ', np.shape(g_calc)
 print 'herm :', herm_grid[0:48]
 
+plt.scatter(growth,freq, color='blue')
+"""
 plt.plot(herm_grid[0:48],g_calc,'b',label = 'calc')
 # try plotting absolute value
 plt.plot(herm_grid[0:48],g_0,'r',label= 'matrix')
