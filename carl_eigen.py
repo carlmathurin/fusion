37947c8248a909a619b7ffb06bec90bf4b0ce11e
Carl M <carl@wireless-10-147-103-110.public.utexas.edu>
2018-11-01 11:45:33 -0500

    fix

--- carl_eigen.py
+++ carl_eigen.py
@@ -59,7 +59,7 @@ for i in range(48):
 print 'herm ',np.shape(herm_grid[0:48]), 'g_calc ', np.shape(g_calc)
 print 'herm :', herm_grid[0:48]
 
-plt.plot(growth,freq,'b*') # color='blue')
+plt.plot(freq,growth,'b*') # color='blue')
 plt.xlabel('real [growth]')
 plt.ylabel('imagainary [freq]')
 plt.show()
