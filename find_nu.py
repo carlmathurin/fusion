e7cd06cde9d4ae6dafe9031847f1b1899ba57e72
Carl M <carl@carls-air.home>
2018-11-27 11:38:29 -0600

    fix

--- find_nu.py
+++ find_nu.py
@@ -27,42 +27,83 @@ par['nu'] = 0.002222
 print 'nu = '  , par['nu']
 
 
+
+
 par['omn'] = 0
 par['omt'] = 0
 par['hyp_x'] = 0
 
-omega, freq , growth, evec = mat.get_spectrum(kx[5],ky[5],kz[5],Gam0[5,5],par['nu'])
 
-max_g = -10
-max_g_i = 0
-
-for i in range(len(growth)):
-    if growth[i] > max_g:
-        max_g = growth[i]
-        max_g_i = i
-
+def max_g(growth):
+    max_g = -10
+    max_g_i = 0
+    for i in range(len(growth)):
+        if growth[i] > max_g:
+            max_g = growth[i]
+            max_g_i = i
+    return max_g, max_g_i
 
 g_0 = evec[:,max_g_i]
 
-nu_bar = par['nu']/kz[5]
-g_calc = np.empty(nmax,dtype=complex)
+def find_error(g_eigen,g_calc,nmax):
+    """"finds the error between g_calc and the eigenvector g"""
+    error = np.empty(nmax,dtype=complex)
 
-for n in range(nmax):
-    if n == 0 or n == 1 :
-        g_calc[0] = g_0[0]
-        g_calc[1] = g_0[1]
-    else:
-        g_calc[n] = ( (omega[max_g_i]/kz[5] + 1j*nu_bar*(n-1))*g_0[n-1] - (n-1)**(.5)*g_0[n-2])/ ((n)**(.5))
-
-error = np.empty(nmax,dtype=complex)
-
-for i in range(nmax):
-    error[i] = abs(g_0[i] - g_calc[i])/ (abs(g_0[i]*g_calc[i])**.5)
-    error2[i] = abs(g_0[i] - g_calc[i])/ (abs(g_0[i]))
-
-def find_error(g_eigen,g_calc):
     for i in range(nmax):
         error[i] = abs(g_eigen[i] - g_calc[i])/ (abs(g_eigen[i]*g_calc[i])**.5)
+
+    return error
+
+def plot_error(error,herm_grid,nmax):
+    plt.plot(herm_grid[0:nmax],error,label= 'err1 [|g_r - g_m| / |g_r*g_m|^.5 ]')
+    plt.legend(loc='top left')
+    plt.xlabel('herm #')
+    plt.ylabel('error')
+    plt.show()
+    return
+
+def plot_eig_vec(g_calc,g_eigen,herm_grid,nmax):
+    plt.plot(herm_grid[0:nmax],abs(g_calc)**2,'b',label = 'calc')
+    plt.plot(herm_grid[0:nmax],abs(g_eigen)**2,'r',label= 'matrix')
+    plt.xlabel('herm #')
+    plt.ylabel('g')
+    plt.title('eigen vector [linear]')
+    plt.legend()
+    plt.show()
+
+    plt.loglog(herm_grid[0:nmax],abs(g_calc)**2,'b',label = 'calc')
+    # try plotting absolute value
+    plt.loglog(herm_grid[0:nmax],abs(g_0)**2,'r',label= 'matrix')
+    plt.xlabel('herm #')
+    plt.ylabel('g')
+    plt.title('eigen vector [logbase(10)]')
+    plt.legend()
+    #label= 'k_p ='+str(k_bin[j])
+    plt.show()
+    return
+
+def calculate_g(kx,ky,kz,nmax,nu,gam0):
+    """calculates g using eigenvector g"""
+
+    omega, freq , growth, evec = mat.get_spectrum(kx,ky,kz,Gam0,nu)
+    g_max, gi_max = max_g(growth)
+    g_0 = evec[:,gi_max]
+
+    nu_bar = nu/kz
+    g_calc = np.empty(nmax,dtype=complex)
+
+    for n in range(nmax):
+        if n == 0 or n == 1 :
+            g_calc[0] = g_0[0]
+            g_calc[1] = g_0[1]
+        else:
+            g_calc[n] = ( (omega[gi_max]/kz + 1j*nu_bar*(n-1))*g_0[n-1] - (n-1)**(.5)*g_0[n-2])/ ((n)**(.5))
+    err = find_error(g_0,g_calc,nmax)
+    plot_eig_vec(g_calc,g_0,herm_grid,nmax)
+    plot_error(err,herm_grid,nmax)
+
+    return g_calc, g_0
+
 
 """
 plt.plot(growth,freq,'b*')
